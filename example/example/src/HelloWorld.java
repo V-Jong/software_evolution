3d0d00b2a7642a73b483ce90f3d3686f5533c271
rmathijn <rocco.mathijnandela@dhl.com>
2017-11-08 14:10:36 +0100

    Add example project

---
+++ example/example/src/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+    
+    public static void secondMethod() {
+    		System.out.println("Do nothing.");
+    }
+    
+    public static void thirdMethod() {
+		System.out.println("Do nothing.");
+    }
+}
